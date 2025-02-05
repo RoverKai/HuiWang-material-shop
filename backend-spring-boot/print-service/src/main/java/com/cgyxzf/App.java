15926f9b432d85cf3d64ea06aa588a907d42a3d1
朱凯佳 <2196140098@qq.com>
2025-02-05 21:11:36 +0800

    initial commit

---
+++ backend-spring-boot/print-service/src/main/java/com/cgyxzf/App.java
@@ -0,0 +1,13 @@
+package com.cgyxzf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
